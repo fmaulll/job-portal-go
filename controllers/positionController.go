a357fb1f3ff2e9d287fd288c80ed62a336956640
fmaulll <maul2821@gmail.com>
2023-02-10 05:32:16 +0700

    final touch

--- controllers/positionController.go
+++ controllers/positionController.go
@@ -17,7 +17,11 @@ func GetPosition(context *gin.Context) {
 	pageStr := context.Query("page")
 	description := context.Query("description")
 	location := context.Query("location")
-	fullTime := context.Query("fullTime")
+	fullTime := context.Query("full_time")
+
+	if fullTime == "true" {
+		fullTime = "Full Time"
+	}
 
 	if pageStr != "" {
 		page, _ = strconv.Atoi(pageStr)
@@ -29,7 +33,7 @@ func GetPosition(context *gin.Context) {
 
 	var position []models.Position
 
-	result := initializers.DB.Where("LOWER(title) LIKE ? AND LOWER(location) LIKE ? AND full_time LIKE ?", description+"%", location+"%", fullTime+"%").Order("title").Limit(10).Offset(offset).Find(&position).Count(&totalRows)
+	result := initializers.DB.Where("LOWER(title) LIKE ? AND LOWER(location) LIKE ? AND type LIKE ?", "%"+description+"%", "%"+location+"%", "%"+fullTime+"%").Order("title").Limit(10).Offset(offset).Find(&position).Count(&totalRows)
 
 	if result.Error != nil {
 		context.JSON(http.StatusNotFound, gin.H{"message": "Position not found"})
@@ -47,7 +51,7 @@ func GetPositionDetail(context *gin.Context) {
 
 	var position models.Position
 
-	result := initializers.DB.First(&position, id)
+	result := initializers.DB.Where("id = ?", id).Find(&position)
 
 	if result.Error != nil {
 		context.JSON(http.StatusNotFound, gin.H{"message": "Position not found"})
@@ -56,34 +60,4 @@ func GetPositionDetail(context *gin.Context) {
 	}
 
 	context.JSON(http.StatusOK, gin.H{"result": position})
-}
-
-func AddPosition(context *gin.Context) {
-	var body struct {
-		Company     string
-		Location    string
-		Title       string
-		Description string
-		Steps       string
-		FullTime    string
-	}
-
-	if context.Bind(&body) != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Failed to read body"})
-
-		return
-	}
-
-	position := models.Position{Company: body.Company, Location: body.Location, Title: body.Title, Description: body.Description, Steps: body.Steps, LogoUrl: "", FullTime: body.FullTime}
-
-	result := initializers.DB.Create(&position)
-
-	if result.Error != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Failed to add employee"})
-
-		return
-
-	}
-
-	context.JSON(http.StatusCreated, gin.H{"message": "Position " + body.Title + " at " + body.Company + " created"})
 }
--- models/positionModel.go
+++ models/positionModel.go
@@ -1,15 +1,15 @@
 package models
 
-import "time"
-
 type Position struct {
-	ID          uint      `gorm:"primaryKey" json:"id"`
-	CreatedAt   time.Time `json:"createdAt"`
-	Company     string    `json:"company"`
-	Location    string    `json:"location"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Steps       string    `json:"steps"`
-	LogoUrl     string    `json:"logoUrl"`
-	FullTime    string    `json:"fullTime"`
+	ID          string `gorm:"primaryKey" json:"id"`
+	Url         string `json:"url"`
+	Type        string `json:"type"`
+	CreatedAt   string `json:"createdAt"`
+	Company     string `json:"company"`
+	CompanyUrl  string `json:"companyUrl"`
+	Location    string `json:"location"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	HowToApply  string `json:"howToApply"`
+	CompanyLogo string `json:"companyLogo"`
 }
